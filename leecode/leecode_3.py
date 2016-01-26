8084ed676ff4c9b6cdfbd62acfab3ba85dc56147
zhangshuo <609955303@qq.com>
2016-01-26 22:11:35 +0800

    commit leecode2 2016.1.26,22:11

--- leecode/leecode_3.py
+++ leecode/leecode_3.py
@@ -1,14 +1,6 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        
+v1 = 453
+v2 = 9
+carry = 2
+carry, val = divmod(v1+v2+carry, 10)
+print carry
+print val
