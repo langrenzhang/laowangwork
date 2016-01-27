8495d93930432e0a762913ecf1e85cbc5d0a668e
zhangshuo <609955303@qq.com>
2016-01-27 23:37:30 +0800

    实验楼数据结构与算法——>链表

--- leecode/leecode_3.py
+++ leecode/leecode_3.py
@@ -1,6 +1,15 @@
-v1 = 453
-v2 = 9
-carry = 2
-carry, val = divmod(v1+v2+carry, 10)
-print carry
-print val
+"""
+Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+
+Subscribe to see which companies asked this question
+
+
+"""
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
---
+++ leecode/test.py
@@ -0,0 +1,27 @@
+#v1 = 453
+#v2 = 9
+#carry = 2
+#carry, val = divmod(v1+v2+carry, 10)
+#print carry
+#print val
+
+#carry = 19
+#carry //=10
+#
+#
+#
+#a = [4,2,456,66,7,3,3,4234,2,5,65,4,2,3,5,3,3,5,2,3,3,66,7,42342,7]
+#print a
+#a = list(sorted(set(a)))
+#print len(a)
+
+
+a = "pwwkew"
+print a
+a = set(a)
+print a
+print a
+print a
+print a
+print a
+print a
